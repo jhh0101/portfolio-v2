02379bfca26417a8701a5627bab713bbd6765ed3
Haehun <jhh010143@gmail.com>
2026-05-31 00:47:03 +0900

    refactor: 도메인 권한 제어(@PreAuthorize) 위임을 통한 Security 관심사 분리 (#5)
    도메인 권한 제어(@PreAuthorize) 위임을 통한 Security 관심사 분리

--- auction-seller-service/src/main/kotlin/org/example/seller/controller/SellerController.kt
+++ auction-seller-service/src/main/kotlin/org/example/seller/controller/SellerController.kt
@@ -9,6 +9,7 @@ import org.example.seller.application.dto.SellerApplyRequest
 import org.example.seller.application.dto.SellerResponse
 import org.example.seller.application.service.SellerService
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
