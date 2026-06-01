abc3e7cb78ef99e5f3b2dcd52446b374113d5e70
Haehun <jhh010143@gmail.com>
2026-06-01 14:38:50 +0900

    refactor: 모놀리식 아키텍처를 MSA 구조로 전환 (#9)
    - 데이터베이스 물리적 격리 (각 서비스별 DB 연결 설정)
    - 공통 모듈(auction-common) 설정을 통한 의존성 및 설정 관리 중앙화
    - 각 서비스별 독립 실행 환경(application.yml) 구축

--- auction-user-service/src/main/kotlin/org/example/user/UserApplication.kt
+++ auction-user-service/src/main/kotlin/org/example/user/UserApplication.kt
@@ -1,4 +1,4 @@
-package org.example.auction.gateway
+package org.example.user
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -8,8 +8,8 @@ import org.springframework.boot.runApplication
         "org.example"
     ]
 )
-class AuctionGatewayApplication
+class UserApplication
 
 fun main(args: Array<String>) {
-    runApplication<AuctionGatewayApplication>(*args)
+    runApplication<UserApplication>(*args)
 }
