8483985a4079dd8cfc75aa1d84c81e511e5df08b
Haehun <jhh010143@gmail.com>
2026-06-01 22:36:35 +0900

    refactor: UserFeignClient 인터페이스 상속 추가 및 컨트롤러 경로 수정 (#16)

--- auction-user-feign/src/main/kotlin/org/example/auction/user/feign/UserFeignClient.kt
+++ auction-user-feign/src/main/kotlin/org/example/auction/user/feign/UserFeignClient.kt
@@ -1,16 +1,22 @@
 package org.example.auction.user.feign
 
+import auction.auctionuserapi.user.client.UserBidClient
 import auction.auctionuserapi.user.client.UserClient
+import auction.auctionuserapi.user.client.UserOrderClient
+import auction.auctionuserapi.user.client.UserSellerClient
 import auction.auctionuserapi.user.dto.UserCommonResponse
+import auction.auctionuserapi.user.type.Role
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(name = "auction-user-service", url = "http://localhost:8087")
 @RequestMapping("/internal/user")
-interface UserFeignClient : UserClient {
+interface UserFeignClient : UserClient, UserBidClient, UserOrderClient, UserSellerClient {
 
     @GetMapping("/{id}")
     override fun userModuleDto(@PathVariable("id") userId: Long) : UserCommonResponse
@@ -29,4 +35,13 @@ interface UserFeignClient : UserClient {
 
     @GetMapping("/{id}/subPoint")
     override fun userSubPoint(@PathVariable("id") userId: Long, bidPrice: Long)
+
+    @GetMapping("/{userId}/validCheck/{bidderId}")
+    override fun userValidCheck(@PathVariable("userId") userId: Long, @PathVariable("bidderId") bidderId: Long)
+
+    @PatchMapping("/{id}/update-rating")
+    override fun updateUserRating(@PathVariable("id") userId: Long, ratingAvg: Double)
+
+    @PatchMapping("/{id}/update-role")
+    override fun userUpdateRole(@PathVariable("id") userId:Long, role: Role)
 }
