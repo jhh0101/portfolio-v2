ae9850220372a910d5eada54319df6d246728c3c
Haehun <jhh010143@gmail.com>
2026-06-02 18:06:13 +0900

    feat: category-feign 모듈 추가
    - 외부 통신 Category 컨트롤러 구현
    - 외부 통신을 위한 Category Feign 모듈 추가

---
+++ auction-category-feign/src/test/kotlin/org/example/auction/category/feign/AuctionCategoryFeignApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.auction.category.feign
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuctionCategoryFeignApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
